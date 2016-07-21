6726e178c45f9ceb6fc9c552c99914454ae4ee0a
Justin Alan Ryan <juryan@ziprealty.com>
2016-07-21 10:37:27 -0700

    force hdfs superusergroup everywhere

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'juryan@ziprealty.com'
 license          'Apache 2.0'
 description      'Installs/Configures zip_hdfs'
 long_description 'Installs/Configures zip_hdfs'
-version          '0.2.5'
+version          '0.2.6'
 
 depends 'java'
 depends 'hadoop'
